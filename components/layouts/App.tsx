95160d669f9c56fd43c3684ebaa881c945528936
Devdreamsolution <q3construction1@gmail.com>
2021-05-11 23:54:59 +0800

    Install prettier for formatting and layouts

---
+++ components/layouts/App.tsx
@@ -0,0 +1,8 @@
+
+const Layout = () => (
+	<div>
+		OK
+	</div>
+);
+
+export default Layout;
---
+++ components/layouts/Page.tsx
@@ -0,0 +1,8 @@
+
+const Layout = (props) => (
+	<div className="bg-red-400 h-screen">
+		{props.children}
+	</div>
+);
+
+export default Layout;
