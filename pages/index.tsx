d06fd8faf50851693e71f3e29225e733e8fa53b3
kcaribou <kcaribou0428@gmail.com>
2021-05-15 00:28:49 +0800

    StepItem Typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,40 @@
-import HeroCard from "../components/landing/HeroCard";
-import NavBar from "../components/landing/NavBar";
-import StepItem from "../components/landing/StepItem";
+import HeroCard from '../components/landing/HeroCard';
+import NavBar from '../components/landing/NavBar';
+import StepItem from '../components/landing/StepItem';
+import AddProjectSvg from '../public/images/landing/add_project.svg';
 
-export default function Landing(): JSX.Element {
-	const steps = [
-		{
-			index: 1,
-			status: true,
-			icon: "../images/landing/add_project.svg",
-			title: "Add Project",
-		},
-		{
-			index: 2,
-			status: true,
-			icon: "../images/landing/add_project.svg",
-			title: "Adjust Area/Size",
-		},
-		{
-			index: 3,
-			status: false,
-			icon: "../images/landing/add_project.svg",
-			title: "Instant Calculation",
-		},
-	];
+export default function Landing() {
 
-	return (
-		<div>
-			<NavBar />
-			<HeroCard />
-			<div className="container mx-auto">
-				<div className="mt-10 sm:mt-20 px-5 sm:px-0 sm:mx-0">
-					<p className="font-bold text-red mb-4 lg:text-4xl md:text-2xl sm:text-xl text-lg">
-						Real Prices On The Spot
-					</p>
-					<p className="text-black sm:text-lg text-sm">
-						Answer a few questions about your current bathroom and
-						renovation goals to receive a detailed, all-inclusive
-						estimate.
-					</p>
-				</div>
-				<div className="mt-8 sm:mt-14 w-full sm:w-1/2">
-					{steps.map((value: iLandingStepType, id: number) => (
-						<StepItem
-							className="my-1"
-							item={value}
-							key={`step-${id}`}
-						/>
-					))}
-				</div>
-				<div className="mt-8 sm:mt-14 w-full sm:w-1/2"></div>
-			</div>
-		</div>
-	);
+  const steps = [
+    {index: 1, status: true, icon: AddProjectSvg, title: "Add Project"},
+    {index: 2, status: false, icon: AddProjectSvg, title: "Adjust Area/Size"},
+    {index: 3, status: false, icon: AddProjectSvg, title: "Instant Calculation"}
+  ];
+
+  return (
+    <div>
+      <NavBar />
+      <HeroCard />
+      <div className="container mx-auto">
+        <div className="mt-10 sm:mt-20 px-5 sm:px-0 sm:mx-0">
+            <p className="font-bold text-red mb-4 lg:text-4xl md:text-2xl sm:text-xl text-lg">Real Prices On The Spot</p>
+            <p className="text-black sm:text-lg text-sm">
+            Answer a few questions about your current bathroom and renovation goals to receive a detailed, all-inclusive estimate.
+            </p>
+        </div>
+        <div className="mt-8 sm:mt-14 w-full sm:w-1/2">
+          {steps.map((value:iLandingStepType, id: number) => (
+            <StepItem
+              className="my-1"
+              item={value}
+              key={`step-${id}`}
+            />
+          ))}
+        </div>
+        <div className="mt-8 sm:mt-14 w-full sm:w-1/2">
+          
+        </div>
+      </div>
+    </div>
+  );
 }
