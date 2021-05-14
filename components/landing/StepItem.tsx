11c2d0034c3b2a22381d1cdbae688b4d240e8ca5
kcaribou <kcaribou0428@gmail.com>
2021-05-15 00:38:16 +0800

    StepItem Typescript

--- components/landing/StepItem.tsx
+++ components/landing/StepItem.tsx
@@ -7,18 +7,22 @@ interface Props {
 function StepItem({ className, item }: Props): JSX.Element {
 	return (
 		<div
-      className={classNames("flex items-center h-16 sm:h-24", className, {
-        "bg-red-lesslight": item.status === true,
-      })}
-    >
-      <span className="inline-flex items-center px-2 sm:px-4 w-8 sm:w-14 h-6 sm:h-10 bg-red text-white font-bold rounded-r-full">
-        {item.index}
-      </span>
-      <item.icon className="h-8 sm:h-14 w-12 ml-2 sm:ml-7 rounded-full" />
-      <p className="text-black font-bold ml-0 sm:ml-4 text-base sm:text-xl">
-        {item.title}
-      </p>
-    </div>
+			className={classNames(
+				"flex items-center h-16 sm:h-24",
+				className,
+				{
+					"bg-red-lesslight": item.status === true,
+				}
+			)}
+		>
+			<span className="inline-flex items-center px-2 sm:px-4 w-8 sm:w-14 h-6 sm:h-10 bg-red text-white font-bold rounded-r-full">
+				{item.index}
+			</span>
+			<item.icon className="h-8 sm:h-14 w-12 ml-2 sm:ml-7 rounded-full" />
+			<p className="text-black font-bold ml-0 sm:ml-4 text-base sm:text-xl">
+				{item.title}
+			</p>
+		</div>
 	);
 }
 
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,9 +1,16 @@
 interface iChildren {
 	children: React.ReactNode;
 }
+
 interface iLandingStepType {
 	status: boolean;
 	index: number;
-	icon: string;
+	icon: ReactElement;
 	title: string;
+}
+
+declare module "*.svg" {
+	import { ReactElement, SVGProps } from "react";
+	const content: (props: SVGProps<SVGElement>) => ReactElement;
+	export default content;
 }
