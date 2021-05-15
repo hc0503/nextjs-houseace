b18d49f2efa1522568eedebafb7a3b950ace6dfb
kcaribou <kcaribou0428@gmail.com>
2021-05-15 08:43:28 +0800

    remove calc page

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,13 +2,6 @@ interface iChildren {
 	children: React.ReactNode;
 }
 
-interface iLandingStepType {
-	status: boolean;
-	index: number;
-	icon: ReactElement;
-	title: string;
-}
-
 declare module "*.svg" {
 	import { ReactElement, SVGProps } from "react";
 	const content: (props: SVGProps<SVGElement>) => ReactElement;
