f46f7c2725fafa2425dca37c403fbf826d06eb89
Devdreamsolution <q3construction1@gmail.com>
2021-05-18 00:18:59 +0800

    Install prisma for ORM

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,9 @@
 interface iChildren {
 	children: React.ReactNode;
 }
-
+interface CustomNodeJsGlobal extends NodeJS.Global {
+	prisma: PrismaClient;
+}
 declare module "*.svg" {
 	import { ReactElement, SVGProps } from "react";
 	const content: (props: SVGProps<SVGElement>) => ReactElement;
