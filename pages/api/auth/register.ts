cfae8f35a2a12545c844983311635bd4e7ace044
Devdreamsolution <q3construction1@gmail.com>
2021-05-24 16:23:03 +0800

    Register with social

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -10,16 +10,24 @@ import { sessionMiddleware } from "@/lib/iron-session";
 export default nc()
 	.use(sessionMiddleware)
 	.post(async (req: any, res: NextApiResponse) => {
-		const { first_name, last_name, email, password } = req.body;
-		const validator = new Validator(req.body, {
-			first_name: "required",
-			last_name: "required",
-			email: "required|email",
-			password: "required|min:6|confirmed",
-			password_confirmation: "required",
-		});
-		if (validator.fails()) {
-			return res.status(412).send(validator.errors);
+		const {
+			first_name,
+			last_name,
+			email,
+			password = "",
+			provider_type = "EMAIL",
+		} = req.body;
+		if (provider_type === "EMAIL") {
+			const validator = new Validator(req.body, {
+				first_name: ["required"],
+				last_name: ["required"],
+				email: ["required", "email"],
+				password: ["required", "min:6", "confirmed"],
+				password_confirmation: ["required"],
+			});
+			if (validator.fails()) {
+				return res.status(412).send(validator.errors);
+			}
 		}
 		try {
 			const user: User = await prisma.user.create({
@@ -28,6 +36,7 @@ export default nc()
 					lastName: last_name,
 					email: email,
 					password: bcrypt.hashSync(password, bcrypt.genSaltSync()),
+					providerType: provider_type,
 				},
 			});
 			req.session.set("user", { user });
@@ -36,6 +45,7 @@ export default nc()
 				.status(201)
 				.json({ msg: "The account is created successfully." });
 		} catch (error) {
+			console.log(error);
 			return res.status(422).send({
 				errors: {
 					email: ["The email already is existed."],
