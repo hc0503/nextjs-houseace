5d67929b125d8d512a72ab991993fae157e7094a
Devdreamsolution <q3construction1@gmail.com>
2021-05-26 17:44:39 +0800

    Change eslint command

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	// Run type-check on changes to TypeScript files
-	'**/*.ts?(x)': () => 'yarn type-check',
+	// Run typecheck on changes to TypeScript files
+	'**/*.ts?(x)': () => 'yarn typecheck',
 	// Run ESLint on changes to JavaScript/TypeScript files
-	'**/*.(ts|js)?(x)': (filenames) => `yarn lint ${filenames.join(' ')}`,
+	'**/*.(ts|js)?(x)': (filenames) => `yarn lintfix ${filenames.join(' ')}`,
 }
