a29175667138585ac1a56baad497d6b840d48d03
Devdreamsolution <q3construction1@gmail.com>
2021-06-01 03:54:32 +0800

    Getting done in landing

--- components/landing/gettingdone/GettingDone.tsx
+++ components/landing/gettingdone/GettingDone.tsx
@@ -1,36 +1,34 @@
+import Slider from "react-slick";
 import GettingItem from "./GettingItem";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const GettingDone: React.FC = (): JSX.Element => {
+	const settings = {
+		className: "center",
+		centerMode: true,
+		infinite: true,
+		centerPadding: "60px",
+		slidesToShow: 4,
+		speed: 500,
+		rows: 2,
+	};
 	return (
-		<>
-			<div className="text-center">
-				<p className="text-4xl font-bold p-3">
-					See what others are getting done
-				</p>
-				<p className="text-sm px-96 py-2">
-					{`We know it sounds unbelievable but it's befinitely possible,
-					over the years we've perfected our pricing system to work to
-					pinpoint accuracy so it generates the most accurate market
-					price for your project.`}
-				</p>
-			</div>
-			<div className="flex space-x-1 space-y-1">
+		<div>
+			<Slider {...settings}>
 				<GettingItem />
 				<GettingItem />
 				<GettingItem />
 				<GettingItem />
 				<GettingItem />
 				<GettingItem />
-			</div>
-			<div className="flex space-x-1 space-y-1">
 				<GettingItem />
 				<GettingItem />
 				<GettingItem />
 				<GettingItem />
-				<GettingItem />
-				<GettingItem />
-			</div>
-		</>
+			</Slider>
+		</div>
 	);
 };
 
--- components/landing/gettingdone/GettingItem.tsx
+++ components/landing/gettingdone/GettingItem.tsx
@@ -4,38 +4,44 @@ import StarRatingComponent from "react-star-rating-component";
 const GettingItem: React.FC = () => {
 	return (
 		<>
-			<div className="p-2 w-72">
+			<div className="p-5 w-80 border rounded-xl shadow-lg">
 				<div className="flex justify-end">
 					<img
-						src="/images/landing/banner.png"
+						src="/images/landing/gettingdone.png"
 						alt=""
-						className="h-10 rounded-md"
+						className="h-14 rounded-md"
 					/>
 				</div>
 				<div className="text-left">
 					<p className="text-lg font-bold">Bathroom Renovation</p>
-					<p className="text-sm">38 McKillop Street, Victoria-3375</p>
+					<p className="text-sm text-gray-light">
+						38 McKillop Street, Victoria-3375
+					</p>
 				</div>
-				<div className="flex space-y-2 py-2 items-center">
-					<img src="/images/users/jene.png" alt="" className="h-10" />
-					<div>
-						<p className="text-sm font-bold">Ashley B Lay</p>
-						<StarRatingComponent
-							name="rate"
-							editing={false}
-							starCount={5}
-							value={5}
+				<div className="flex items-center justify-between">
+					<div className="flex items-center space-x-2 py-2">
+						<img
+							src="/images/users/Jene.png"
+							alt="Avatar"
+							className="h-10"
 						/>
+						<div>
+							<p className="text-sm font-bold">Ashley B Lay</p>
+							<StarRatingComponent
+								name="rate"
+								editing={false}
+								starCount={5}
+								value={5}
+							/>
+						</div>
 					</div>
-					<div className="">
-						<ArrowCircleButton
-							arrowSize={4}
-							fontsize="text-xs"
-							padding="px-1"
-						>
-							Instant Quote
-						</ArrowCircleButton>
-					</div>
+					<ArrowCircleButton
+						arrowSize="h-4 w-4"
+						fontsize="text-xs"
+						padding="px-1"
+					>
+						Instant Quote
+					</ArrowCircleButton>
 				</div>
 			</div>
 		</>
