41e5f1ff6153a6101e14b804ab001a3d7313e771
Devdreamsolution <q3construction1@gmail.com>
2021-05-31 08:52:49 +0800

    Add introduce

---
+++ components/landing/introduce/Introduce.tsx
@@ -0,0 +1,28 @@
+import IntroduceItem from "./IntroduceItem";
+
+const Introduce: React.FC = () => (
+	<div className="flex justify-between items-start p-28">
+		<IntroduceItem
+			src="/images/landing/entrepreneur.png"
+			alt="Entrepreneur"
+			description="Houseace utilizes tech to make renovations easier to arrange and understand"
+		/>
+		<IntroduceItem
+			src="/images/landing/homely.png"
+			alt="Homely"
+			description="Visit houseace for free, online renovation quotes in just 2 minutes."
+		/>
+		<IntroduceItem
+			src="/images/landing/know_techie.png"
+			alt="Know Techie"
+			description="Houseace brings the power of technology to home renovations."
+		/>
+		<IntroduceItem
+			src="/images/landing/lendi.png"
+			alt="Lendi"
+			description="Now it's time to get an online renovation quote."
+		/>
+	</div>
+);
+
+export default Introduce;
