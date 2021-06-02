577bc01e33b4cb32d75f52c701392fac6f5724fb
Devdreamsolution <q3construction1@gmail.com>
2021-06-03 01:56:39 +0800

    Update Tailwind setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,33 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or "media" or "class"
   theme: {
     extend: {
       colors: {
         red: {
-          lesslight: '#FFE8E8',
-          light: '#F87171',
-          DEFAULT: '#F96868',
-          dark: '#EF4444',
-          moredark: '#DC2626'
+          lesslight: "#FFE8E8",
+          light: "#F87171",
+          DEFAULT: "#F96868",
+          dark: "#EF4444",
+          moredark: "#DC2626"
         },
         gray: {
-          lesslight: '#E5E7EB',
-          light: '#959595',
-          DEFAULT: '#616161',
-          dark: '#161616',
-          moredark: '#4B5563'
+          lesslight: "#E5E7EB",
+          light: "#959595",
+          DEFAULT: "#616161",
+          dark: "#161616",
+          moredark: "#4B5563"
+        },
+        custom: {
+          1: "#D5D2C8"
         }
       },
       maxWidth: {
-        message: '919px',
+        message: "919px",
       },
+      backgroundImage: {
+        "hero-pattern": "url('/images/landing/banner.png')",
+      }
     },
   },
   variants: {
