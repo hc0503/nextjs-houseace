b1b24065bc395d5f4b3db1a16e611085904256ea
Devdreamsolution <q3construction1@gmail.com>
2021-06-02 10:36:20 +0800

    Add auth page title

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,18 +1,25 @@
+import Head from "next/head";
+
 import Layout from "@/components/layout/Page";
 import AuthBackground from "@/components/auth/AuthBackground";
 import LoginForm from "@/components/auth/LoginForm";
 
 const Login: React.FC = (): JSX.Element => {
 	return (
-		<Layout>
-			<div className="min-h-screen bg-red flex">
-				<LoginForm />
-				<AuthBackground
-					background="/images/auth/bg_login.png"
-					alt="Login background"
-				/>
-			</div>
-		</Layout>
+		<>
+			<Head>
+				<title>{`${process.env.APP_NAME} - Login`}</title>
+			</Head>
+			<Layout>
+				<div className="min-h-screen bg-red flex">
+					<LoginForm />
+					<AuthBackground
+						background="/images/auth/bg_login.png"
+						alt="Login background"
+					/>
+				</div>
+			</Layout>
+		</>
 	);
 };
 
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,15 +1,22 @@
+import Head from "next/head";
+
 import Layout from "@/components/layout/Page";
 import AuthBackground from "@/components/auth/AuthBackground";
 import RegisterForm from "@/components/auth/RegisterForm";
 
 const Register: React.FC = (): JSX.Element => {
 	return (
-		<Layout>
-			<div className="min-h-screen bg-red flex">
-				<RegisterForm />
-				<AuthBackground background="/images/auth/bg_register.png" />
-			</div>
-		</Layout>
+		<>
+			<Head>
+				<title>{`${process.env.APP_NAME} - Register`}</title>
+			</Head>
+			<Layout>
+				<div className="min-h-screen bg-red flex">
+					<RegisterForm />
+					<AuthBackground background="/images/auth/bg_register.png" />
+				</div>
+			</Layout>
+		</>
 	);
 };
 
