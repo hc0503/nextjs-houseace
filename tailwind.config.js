be51e2bfef680ef9c509617a3dac49aaf9846a5f
Devdreamsolution <q3construction1@gmail.com>
2021-06-08 16:25:31 +0800

    Install react hook validation and remove Tailwind form

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,6 +54,7 @@ module.exports = {
     },
   },
   plugins: [
-    require("@tailwindcss/forms")
+    // This makes outline in elements like input which type is password.
+    // require("@tailwindcss/forms")
   ],
 }
