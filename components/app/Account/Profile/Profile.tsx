aad301042c3441c93a211700976f8a2ae831865a
Devdreamsolution <q3construction1@gmail.com>
2021-06-08 21:17:03 +0800

    Profile info card responsive

--- components/app/Account/Profile/Profile.tsx
+++ components/app/Account/Profile/Profile.tsx
@@ -19,8 +19,8 @@ const Profile: React.FC = (): JSX.Element => {
 	return (
 		<>
 			{/* User info card */}
-			<div className="flex focus:outline-none bg-red shadow rounded-2xl mt-5 md:px-28 px-2 md:py-8 py-2">
-				<div className="md:flex items-center md:space-x-20 w-full">
+			<div className="flex focus:outline-none bg-red shadow rounded-2xl mt-5 px-2 md:py-8 py-2">
+				<div className="grid md:grid-cols-3 grid-cols-1 items-center relative">
 					{/* Avatar */}
 					<div className="flex-grow lg:mt-0 lg:ml-6 lg:flex-grow-0 lg:flex-shrink-0">
 						<div className="mt-1 lg:hidden">
@@ -39,7 +39,7 @@ const Profile: React.FC = (): JSX.Element => {
 									<div className="group relative border border-gray-300 rounded-md py-2 px-3 flex items-center justify-center hover:bg-gray-50 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-light-blue-500">
 										<label
 											htmlFor="user_photo"
-											className="relative text-sm leading-4 font-medium text-gray-700 pointer-events-none"
+											className="relative text-sm leading-4 font-medium text-gray-700"
 										>
 											<span>Change</span>
 											<span className="sr-only"> user photo</span>
@@ -55,61 +55,62 @@ const Profile: React.FC = (): JSX.Element => {
 							</div>
 						</div>
 
-						<div className="hidden relative rounded-full overflow-hidden lg:block">
-							<img
-								className="relative rounded-full w-40 h-40"
-								src={user.imageUrl}
-								alt=""
-							/>
-							<label
-								htmlFor="user-photo"
-								className="absolute inset-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center text-sm font-medium text-white opacity-0 hover:opacity-100 focus-within:opacity-100"
-							>
-								<span>Change</span>
-								<span className="sr-only"> user photo</span>
-								<input
-									type="file"
-									id="user-photo"
-									name="user-photo"
-									className="absolute inset-0 w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"
+						<div className="flex items-center justify-center">
+							<div className="hidden relative rounded-full overflow-hidden lg:block w-52 h-52">
+								<img
+									className="relative rounded-full"
+									src={user.imageUrl}
+									alt=""
 								/>
-							</label>
+								<label
+									htmlFor="user-photo"
+									className="absolute inset-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center text-sm text-white opacity-0 hover:opacity-100"
+								>
+									<span>Change</span>
+									<span className="sr-only"> user photo</span>
+									<input
+										type="file"
+										id="user-photo"
+										name="user-photo"
+										className="absolute inset-0 w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"
+									/>
+								</label>
+							</div>
 						</div>
 					</div>
 
-					<div className="w-full h-full md:flex items-stretch flex-wrap md:mt-0 mt-2 relative">
-						<div className="md:flex justify-between items-center w-full">
-							<div>
-								<InfoLabel
-									label="Full Name"
-									value={user.fullName}
-									icon={<FiUser className="h-4 w-4" />}
-								/>
-							</div>
-							<div className="md:mt-0 mt-2">
-								<InfoLabel
-									label="Phone Number"
-									value={user.phoneNumber}
-									icon={<BiPhoneCall className="h-4 w-4" />}
-								/>
-							</div>
+					<div className="h-full md:flex items-stretch flex-wrap mt-10">
+						<div>
+							<InfoLabel
+								label="Full Name"
+								value={user.fullName}
+								icon={<FiUser className="h-4 w-4" />}
+							/>
 						</div>
-						<div className="flex items-center md:mt-0 mt-2">
+						<div className="flex items-center md:mt-0 mt-4">
 							<InfoLabel
 								label="Address"
 								value={user.address}
 								icon={<GoLocation className="h-4 w-4" />}
 							/>
 						</div>
-						<div className="absolute md:-right-24 md:-top-4 right-0 -top-14">
-							<button className="focus:outline-none hover:text-red-lesslight text-white">
-								<FiEdit className="w-7 h-7" />
-							</button>
-						</div>
+					</div>
+
+					<div className="h-full">
+						<InfoLabel
+							label="Phone Number"
+							value={user.phoneNumber}
+							icon={<BiPhoneCall className="h-4 w-4" />}
+						/>
+					</div>
+					<div className="absolute right-0 md:-top-6 top-0">
+						<button className="focus:outline-none hover:text-red-lesslight text-white">
+							<FiEdit className="w-7 h-7" />
+						</button>
 					</div>
 				</div>
 			</div>
-			<div className="grid grid-cols-2 mt-6 space-x-5">
+			<div className="grid grid-cols-2 mt-6">
 				<div className="overflow-y-auto focus:outline-none bg-white shadow rounded-2xl pl-10 py-11">
 					<div>
 						<p className="font-bold text-2xl">Password Setting</p>
