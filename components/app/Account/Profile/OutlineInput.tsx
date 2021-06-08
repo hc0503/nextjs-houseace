586dbb841c755f9c01197bd5e16e4eaecc02b15b
Devdreamsolution <q3construction1@gmail.com>
2021-06-08 16:27:31 +0800

    Make outline input component

---
+++ components/app/Account/Profile/OutlineInput.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames";
+
+interface IProps extends IInput {
+	children: React.ReactNode;
+}
+
+const OutlineInput: React.FC<IProps> = ({
+	children,
+	id,
+	name,
+	placeHolder,
+	type,
+}): JSX.Element => {
+	return (
+		<div className="mt-1 focus-within:text-gray text-gray-light relative">
+			<div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+				{children}
+			</div>
+			<input
+				id={id}
+				name={name}
+				placeholder={placeHolder}
+				type={type}
+				className={classNames(
+					"block w-full px-3 pl-10 py-2 placeholder-gray-light focus:outline-none border-b-2 focus:border-red"
+				)}
+			/>
+		</div>
+	);
+};
+
+export default OutlineInput;
