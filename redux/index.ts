eb93f042e60b0857f264b161f4889f97b96c2da8
Devdreamsolution <q3construction1@gmail.com>
2021-06-09 21:29:38 +0800

    Role redux

--- redux/index.ts
+++ redux/index.ts
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 
 import authSlice from "./slices/authSlice";
+import roleSlice from "./slices/roleSlice";
 
 const store = configureStore({
 	reducer: {
 		auth: authSlice,
+		role: roleSlice,
 	},
 });
 
