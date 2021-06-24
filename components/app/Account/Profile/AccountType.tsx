ff524096a8e09fdd4a9e54cf45f59a9589df9515
Devdreamsolution <q3construction1@gmail.com>
2021-06-24 22:52:27 +0800

    Fix fetch profile data

--- components/app/Account/Profile/AccountType.tsx
+++ components/app/Account/Profile/AccountType.tsx
@@ -14,7 +14,7 @@ const AccountType: React.FC = (): JSX.Element => {
 		dispatch(fetchRoleList());
 	}, []);
 	const profileData: IUser = useSelector(
-		(state: any) => state.auth.data.user
+		(state: any) => state.profile.data
 	);
 	const roleList: IRole[] = useSelector(
 		(state: any) => state.role.data
@@ -26,7 +26,7 @@ const AccountType: React.FC = (): JSX.Element => {
 			successToast("update successfully.");
 		}
 	);
-
+	console.log(profileData);
 	return (
 		<div className="focus:outline-none bg-white shadow rounded-2xl xl:pl-10 pl-5 pt-11 pb-9 pr-1">
 			{(!profileData || !roleList) && (
