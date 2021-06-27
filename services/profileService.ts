34b438397c43829c3ca901e3bf3e61dc251b34e5
Devdreamsolution <q3construction1@gmail.com>
2021-06-27 11:02:02 +0800

    Change profile update data route

--- services/profileService.ts
+++ services/profileService.ts
@@ -26,7 +26,7 @@ export const postUpdateProfileData = async (
 ): Promise<any> => {
 	try {
 		const res = await axios.post(
-			"/api/v1/private/profiles/me/update",
+			"/api/v1/private/profiles/me/update-data",
 			data
 		);
 		return Promise.resolve(res.data);
